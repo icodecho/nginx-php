149f2297f03808a925db255472db920bb41beee5
icodecho <9eeks.lover@gmail.com>
2016-03-21 21:22:16 +0800

    Update myfile.sh

--- myfile.sh
+++ myfile.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-if [ "$MY_WGET_FILE_URI" = "" ] then
-echo "the environment[MY_WGET_FILE_URI] not set. Exiting."
-exit 1
-else
-rm -f /var/www/*
+rm -rf /var/www/*
 wget -P /var/www/ http://$MY_WGET_FILE_URI/$MY_WGET_FILE_NAME
 echo "========================================================================"
 echo ""
@@ -12,5 +8,4 @@ echo "==the file is '$MY_WGET_FILE_NAME'=="
 echo ""
 echo "========================================================================"
 unzip /var/www/$MY_WGET_FILE_NAME -d /var/www/
-rm -f /var/www/$MY_WGET_FILE_NAME
-fi
+rm -rf /var/www/$MY_WGET_FILE_NAME
